9e8859d0aa9b9ea84072e3ba7dd3973b99c84f3d
priyampinju <pinjupriyam1826@gmail.com>
2024-02-04 22:58:36 +0530

    mobile header done

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -5,7 +5,7 @@ import Button from '../../common/button/Button'
 const HeroSection = () => {
   return (
     <div className='hero-section-wrapper'>
-      <div className="flex absolute-center flex-col max-width hero-section">
+      <div className="hero-section flex absolute-center flex-col max-width">
         <div className='hero-heading'> rewards for paying credit card bills. </div>
         <div className="hero-subheading">join 9M+ members who win rewards and cashbacks everyday</div>
         <Button buttonText='Download CRED'  />
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection/HeroSection'
+import Header from '../components/HeroSection/header/Header'
 
 const Homepage = () => {
   return (
-    <div>
+    <>
+        <Header />
       <HeroSection />
-    </div>
+    </>
   )
 }
 
