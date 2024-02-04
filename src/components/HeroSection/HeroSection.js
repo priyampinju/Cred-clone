4243b42b8543c0064440b135966e1f57dce893b7
priyampinju <pinjupriyam1826@gmail.com>
2024-02-04 16:54:35 +0530

    sdf

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './heroSection.css'
+import Button from '../../common/button/Button'
 
 const HeroSection = () => {
   return (
     <div className='hero-section-wrapper'>
-      <div className="flex absolute-center flex-col max-width">
-        JDbsdbfldjbjdsbfskdfbsdkfsdbfsdfsdfsdfsdfsdsdfsdfsdfsfsdfsdfsdfsddsf
+      <div className="flex absolute-center flex-col max-width hero-section">
+        <div className='hero-heading'> rewards for paying credit card bills. </div>
+        <div className="hero-subheading">join 9M+ members who win rewards and cashbacks everyday</div>
+        <Button buttonText='Download CRED'  />
       </div>
 
     </div>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection/HeroSection'
-import Header from '../components/Header/Header'
 
 const Homepage = () => {
   return (
     <div>
-        <Header />
       <HeroSection />
     </div>
   )
