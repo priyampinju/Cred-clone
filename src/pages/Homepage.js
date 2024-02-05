92bea6ccfa33999681cd573c526a484e5c655f46
priyampinju <pinjupriyam1826@gmail.com>
2024-02-05 17:33:40 +0530

    mobile sections done

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection/HeroSection'
 import Header from '../components/HeroSection/header/Header'
+import ProductShowcase from '../components/ProductShowcase/ProductShowcase'
+import FeelSpecial from '../common/feelSpecial/FeelSpecial'
 
 const Homepage = () => {
-  return (
-    <>
-        <Header />
-      <HeroSection />
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <HeroSection />
+            <ProductShowcase />
+            <FeelSpecial />
+        </>
+    )
 }
 
 export default Homepage
